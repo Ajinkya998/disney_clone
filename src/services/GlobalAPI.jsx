56baeaa9389ecbbf1f71d5a9aaba38df4defd97a
Ajinkya998 <ajinkyakharujkar998@gmail.com>
2025-06-11 00:09:11 +0530

    Changes

--- src/services/GlobalAPI.jsx
+++ src/services/GlobalAPI.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const moviesBaseURL = 'https://api.themoviedb.org/3';
 const movieByGenreBaseURL = 'https://api.themoviedb.org/3/discover/movie?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf';
-const token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NGQzNzYzODNhZDkxMDRkNjU4NjRmYzRkOWM5MmUwNiIsIm5iZiI6MTc0OTM4NTE4OS4wMjMsInN1YiI6IjY4NDU3ZmU1YmMwNGJmOWJiNjNmYzQyMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Fucepg-hup47_agWa5uO-16U_TOcB-0sotnCFNgF7Pw";
+
 
 const getTrendingVideos = axios.get(`${moviesBaseURL}/trending/all/day`, {
     headers: {
